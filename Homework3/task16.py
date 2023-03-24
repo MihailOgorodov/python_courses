209cfefdff2641a52d6a905c61673ec4aba194fd
“Mihail <mic311089@gmail.com>
2023-03-24 09:45:16 +0400

    task16 readded

--- Homework3/task16.py
+++ Homework3/task16.py
@@ -7,13 +7,20 @@
 #     3
 #     -> 1
 
+# import random
+# count_list = int(input('Введите количество элементов: '))
+# first_list = [random.randint(1,20) for i in range(count_list)]
+# print(*first_list)
+# number = int(input('Введите число: '))
+# count = 0
+# for i in range(len(first_list)):
+#     if first_list[i] == number:
+#         count += 1
+# print(f'Число {number} встречается {count} раз')
+
 import random
 count_list = int(input('Введите количество элементов: '))
-first_list = [random.randint(1,20) for i in range(count_list)]
-print(*first_list)
+first_list = [random.randint(0,20) for _ in range(count_list)]
+print(first_list)
 number = int(input('Введите число: '))
-count = 0
-for i in range(len(first_list)):
-    if first_list[i] == number:
-        count += 1
-print(f'Число {number} встречается {count} раз')
+print(f'Число {number} встречается в списке {first_list.count(number)} раз')
