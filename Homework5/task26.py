b9eac04b1e1415ca12eee2ebb79b1456d8367b2d
“Mihail <mic311089@gmail.com>
2023-03-24 16:05:50 +0400

    task26 added

--- Homework5/task26.py
+++ Homework5/task26.py
@@ -1,9 +1,13 @@
 # Задача 26:  Напишите программу, которая на вход принимает два числа A и B, и возводит число А в
 # целую степень B с помощью рекурсии.
+number = int(input('Введите число: '))
+number_degree = int(input('Введите степень: '))
+
 
 def degree(num1: int, num2: int) -> int:
     if num2 == 0:
         return 1
     return num1 * degree(num1, num2-1)
 
-print(degree(3,3))
+
+print(degree(number, number_degree))
