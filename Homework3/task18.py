c51e006435286758f6d5be1adb4ec0a4c24914cf
“Mihail <mic311089@gmail.com>
2023-03-24 10:04:34 +0400

    task18 readded

--- Homework3/task18.py
+++ Homework3/task18.py
@@ -6,12 +6,28 @@
 #     1 2 3 4 5
 #     6
 #     -> 5
+# import random
+#
+# count_list = int(input('Введите количество элементов: '))
+# first_list = [random.randint(1, 20) for i in range(count_list)]
+# print(*first_list)
+# number = int(input('Введите число: '))
+# temp = 0
+# find_num = [abs(first_list[i] - number) for i in range(len(first_list))]
+# print(first_list[find_num.index(min(find_num))])
 import random
 
-count_list = int(input('Введите количество элементов: '))
-first_list = [random.randint(1, 20) for i in range(count_list)]
-print(*first_list)
+my_list = [random.randint(0, 100) for _ in range(20)]
+print(my_list)
 number = int(input('Введите число: '))
-temp = 0
-find_num = [abs(first_list[i] - number) for i in range(len(first_list))]
-print(first_list[find_num.index(min(find_num))])
+nearest_number = my_list[0]
+dist = abs(number - nearest_number)
+for num in my_list:
+    if abs(num - number) < dist:
+        dist = abs(num - number)
+        nearest_number = num
+
+if my_list.count(number):
+    print(f'Число {number} встречается в списке {my_list.count(number)} раз')
+else:
+    print(f'Ближайшее число к {number} является {nearest_number}')
